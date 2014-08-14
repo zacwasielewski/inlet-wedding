00bce242f2705332e36d878908a79e9ce3d819d7
Zac Wasielewski <zac@wasielewski.org>
2014-08-13 20:57:02 -0400

    Increase bottom margin of H1 elements

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99b6fc40d3c506181034dccc5bb654b0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '34a86dd487558eaf7d5c7b5fe1a6e20f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
