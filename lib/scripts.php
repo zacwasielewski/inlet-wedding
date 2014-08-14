8cd104cbb8e8f2f0e3737bf0469ca0c9b0b281f4
Zac Wasielewski <zac@wasielewski.org>
2014-08-13 20:46:36 -0400

    Add background image for 'Directions' section on home page and modify header font weights

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9a374d71dbd0544105d8870e0f4b0f7c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99b6fc40d3c506181034dccc5bb654b0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
